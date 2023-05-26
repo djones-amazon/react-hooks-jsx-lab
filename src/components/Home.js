8f398bf4afc0038c45edc3ff8b2238f72a6fb9eb
Devin Jones <devin.jones21083@gmail.com>
2023-05-26 18:16:35 -0400

    Finished all deliverables

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,16 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const headingPhrase = `${name} is a Web Developer from ${city}`;
+  
+  //console.log(headingPhrase);
+  return (
+  <div id='home'>
+    <h1 style={{color: 'firebrick'}}>
+      {headingPhrase}
+    </h1>
+  </div>
+  )
 }
 
 export default Home;
